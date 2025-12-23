1b1ad1ed97ff41fc5808c729dd3d9460e4be2a04
jaimehdd <hieudo243@gmail.com>
2025-12-24 02:12:13 +0800

    feat: update Reddit patching to arsclip

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -53,18 +53,23 @@ revanced_dl(){
 	patch "youtube-music-arm64-v8a" "revanced-extended" "inotia"
 }
 3() {
-	revanced_dl
+	dl_gh "revanced-patches-arsclib revanced-integrations revanced-cli-arsclib" "inotia00" "latest"
 	# Patch Reddit:
 	echo "REPO_NAME=reddit-rv" >> $GITHUB_ENV
-	get_patches_key "reddit-rve"
-	version="2025.51.0"
+	get_patches_key "reddit-rve-arsclib"
 	get_apk "com.reddit.frontpage" "reddit" "reddit" "redditinc/reddit/reddit" "Bundle_extract"
 	split_editor "reddit" "reddit"
-	patch "reddit" "revanced-extended" "inotia"
+	patch "reddit" "revanced-extended"
+	mv ./release/reddit-revanced-extended.apk/base.apk ./reddit-revanced-extended.apk
+	rm -f -d ./release/reddit-revanced-extended.apk
+	mv ./reddit-revanced-extended.apk ./release/reddit-revanced-extended-arsclib.apk
 	# Patch Arm64-v8a:
-	split_editor "reddit" "reddit-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
-	get_patches_key "reddit-rve"
-	patch "reddit-arm64-v8a" "revanced-extended" "inotia"
+	get_patches_key "reddit-rve-arsclib"
+	split_editor "reddit" "reddit-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xxhdpi split_config.tvdpi"
+	patch "reddit-arm64-v8a" "revanced-extended"
+	mv ./release/reddit-arm64-v8a-revanced-extended.apk/base.apk ./reddit-arm64-v8a-revanced-extended.apk
+	rm -f -d ./release/reddit-arm64-v8a-revanced-extended.apk
+	mv ./reddit-arm64-v8a-revanced-extended.apk ./release/reddit-arm64-v8a-revanced-extended-arsclib.apk
 }
 case "$1" in
     1)

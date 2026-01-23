da40102c6eaf08e0b46d5234b87f168e6b259a08
jaimehdd <hieudo243@gmail.com>
2026-01-23 23:10:40 +0800

    fix: add rvxposed patch_version

--- src/build/ReVancedXposed.sh
+++ src/build/ReVancedXposed.sh
@@ -8,8 +8,17 @@ dl_gh "ReVancedXposed_Spot"$j"fy" "chsbuffer" "latest"
 dl_gh "LSPatch" "JingMatrix" "latest"
 #################################################
 
+# Extract ReVancedXposed version from the downloaded APK filename
+xposed_apk=$(ls ReVancedXposed*.apk 2>/dev/null | head -n1)
+if [[ -n "$xposed_apk" ]]; then
+    xposed_version=$(echo "$xposed_apk" | sed -E 's/ReVancedXposed[^0-9]*([0-9.]+)\.apk/\1/')
+else
+    xposed_version="unknown"
+fi
+
 echo "APP_NAME=spotify" >> $GITHUB_ENV
 echo "VARIANT=rv" >> $GITHUB_ENV
+echo "patch_version=$xposed_version" >> $GITHUB_ENV
 
 # Patch Spotjfy:
 get_apkpure "com.spot"$j"fy.music" "spotjfy" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"

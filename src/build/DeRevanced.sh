66cac470e310bb1554885f842bb4be1e733e66dc
jaimehdd <hieudo243@gmail.com>
2026-04-04 17:58:41 +0800

    feat: add De-Revanced

---
+++ src/build/DeRevanced.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# DeRevanced build
+source ./src/build/utils.sh
+# Download requirements
+use_beta="${use_beta:-false}"
+
+derevanced_dl_beta(){
+	dl_gh "morphe-cli" "MorpheApp" "prerelease"
+	dl_gh "De-ReVanced" "RookieEnough" "prerelease"
+}
+
+derevanced_dl(){
+	if [ "$use_beta" = true ]; then
+		derevanced_dl_beta
+	else
+		dl_gh "morphe-cli" "MorpheApp" "latest"
+		dl_gh "De-ReVanced" "RookieEnough" "latest"
+	fi
+}
+
+messenger() {
+	derevanced_dl
+	# Patch Messenger:
+	# Arm64-v8a
+	get_patches_key "messenger-derevanced"
+	version="552.0.0.44.65"
+	get_apkpure "com.facebook.orca" "messenger-arm64-v8a" "facebook-messenger/com.facebook.orca"
+	patch "messenger-arm64-v8a" "derevanced" "morphe"
+}
+
+photos() {
+	derevanced_dl
+	# Patch Google photos:
+	# Arm64-v8a
+	get_patches_key "gg-photos-derevanced"
+	get_apk "com.google.android.apps.photos" "gg-photos-arm64-v8a" "photos" "google-inc/photos/google-photos" "arm64-v8a" "nodpi"
+	patch "gg-photos-arm64-v8a" "derevanced" "morphe"
+}
+
+facebook() {
+	derevanced_dl
+	# Patch Facebook:
+	# Arm64-v8a
+	get_patches_key "facebook-derevanced"
+	url="https://d.apkpure.com/b/APK/com.facebook.katana?versionCode=457020014"
+	req "$url" "facebook-arm64-v8a.apk"
+	patch "facebook-arm64-v8a" "derevanced" "morphe"
+}
+
+strava() {
+	derevanced_dl
+	# Patch Strava:
+	get_patches_key "strava-derevanced"
+	get_apkpure "com.strava" "strava-arm64-v8a" "strava-run-hike-android-exercise-laugh/com.strava" "Bundle"
+	patch "strava-arm64-v8a" "derevanced" "morphe"
+}
+
+case "$1" in
+    messenger)
+        messenger
+        ;;
+    photos)
+        photos
+        ;;
+    facebook)
+        facebook
+        ;;
+    strava)
+        strava
+        ;;
+esac

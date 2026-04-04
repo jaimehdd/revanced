22703b7011b7b4a7494195adb3213980af46a8cd
jaimehdd <hieudo243@gmail.com>
2026-04-04 18:52:25 +0800

    feat: add instagram-Piko

--- src/build/DeRevanced.sh
+++ src/build/DeRevanced.sh
@@ -24,7 +24,7 @@ messenger() {
 	echo "APP_NAME=messenger" >> $GITHUB_ENV
 	echo "VARIANT=rv" >> $GITHUB_ENV
 	# Arm64-v8a
-	get_patches_key "messenger-derevanced"
+	get_patches_key "messenger"
 	version="552.0.0.44.65"
 	get_apkpure "com.facebook.orca" "messenger-arm64-v8a" "facebook-messenger/com.facebook.orca"
 	patch "messenger-arm64-v8a" "derevanced" "morphe"
@@ -36,9 +36,20 @@ photos() {
 	echo "APP_NAME=google-photos" >> $GITHUB_ENV
 	echo "VARIANT=rv" >> $GITHUB_ENV
 	# Arm64-v8a
-	get_patches_key "gg-photos-derevanced"
+	get_patches_key "gg-photos"
 	get_apk "com.google.android.apps.photos" "gg-photos-arm64-v8a" "photos" "google-inc/photos/google-photos" "arm64-v8a" "nodpi"
 	patch "gg-photos-arm64-v8a" "derevanced" "morphe"
+}
+
+instagram() {
+	dl_gh "morphe-cli" "MorpheApp" "latest"
+	dl_gh "piko" "crimera" "prerelease"
+	# Patch Instagram:
+	echo "APP_NAME=instagram" >> $GITHUB_ENV
+	echo "VARIANT=rv" >> $GITHUB_ENV
+	get_patches_key "instagram"
+	get_apkpure "com.instagram.android" "instagram-arm64-v8a" "instagram-android/com.instagram.android" "Bundle"
+	patch "instagram-arm64-v8a" "piko" "morphe"
 }
 
 facebook() {
@@ -47,7 +58,7 @@ facebook() {
 	echo "APP_NAME=facebook" >> $GITHUB_ENV
 	echo "VARIANT=rv" >> $GITHUB_ENV
 	# Arm64-v8a
-	get_patches_key "facebook-derevanced"
+	get_patches_key "facebook"
 	version="490.0.0.63.82"
 	echo "APP_VERSION=$version" >> $GITHUB_ENV
 	url="https://d.apkpure.com/b/APK/com.facebook.katana?versionCode=457020014"
@@ -60,22 +71,25 @@ strava() {
 	# Patch Strava:
 	echo "APP_NAME=strava" >> $GITHUB_ENV
 	echo "VARIANT=rv" >> $GITHUB_ENV
-	get_patches_key "strava-derevanced"
+	get_patches_key "strava"
 	get_apkpure "com.strava" "strava-arm64-v8a" "strava-run-hike-android-exercise-laugh/com.strava" "Bundle"
 	patch "strava-arm64-v8a" "derevanced" "morphe"
 }
 
 case "$1" in
-    messenger)
-        messenger
-        ;;
-    photos)
-        photos
-        ;;
-    facebook)
-        facebook
-        ;;
-    strava)
-        strava
-        ;;
+	messenger)
+		messenger
+		;;
+	photos)
+		photos
+		;;
+	instagram)
+		instagram
+		;;
+	facebook)
+		facebook
+		;;
+	strava)
+		strava
+		;;
 esac

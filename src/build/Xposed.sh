1f2d097902375c61ea0f42e3eb9adfc483822fc0
jaimehdd <hieudo243@gmail.com>
2026-05-12 23:48:15 +0800

    feat: disable play store update for lspatch

--- src/build/Xposed.sh
+++ src/build/Xposed.sh
@@ -6,6 +6,26 @@ LSPatch_dl(){
 	dl_gh "LSPatch" "JingMatrix" "latest"
 }
 
+xposed_morphe_universal_dl() {
+	dl_gh "morphe-patches" "MorpheApp" "latest"
+	for patches_file in patches-*.mpp; do
+		[ -e "$patches_file" ] || continue
+		mv "$patches_file" "morphe-universal-$patches_file.disabled"
+	done
+}
+
+xposed_dl() {
+	LSPatch_dl
+	dl_gh "morphe-cli" "MorpheApp" "latest"
+	xposed_morphe_universal_dl
+	dl_gh "NexAlloy" "gnadgnaoh" "prerelease"
+	echo "patch_version=1" >> $GITHUB_ENV
+}
+
+xposed_disable_play_store_updates() {
+	morphe_disable_play_store_updates "$1" "gnadgnaoh-lspatched"
+}
+
 facebook() {
     APP_NAME="facebook"
 	VARIANT="xposed"
@@ -13,11 +33,11 @@ facebook() {
 	echo "VARIANT=$VARIANT" >> $GITHUB_ENV
 	echo "patch_version=1" >> $GITHUB_ENV
 
-	LSPatch_dl
-	dl_gh "NexAlloy" "gnadgnaoh" "prerelease"
+	xposed_dl
 	# Patch Facebook:
 	get_apk "com.facebook.katana" "facebook-arm64-v8a" "bundle" "arm64-v8a" "nodpi" "Android 9+"
 	lspatch "facebook-arm64-v8a" "NexAlloy*.apk" "gnadgnaoh"
+	xposed_disable_play_store_updates "facebook-arm64-v8a"
 }
 
 messenger() {
@@ -27,11 +47,11 @@ messenger() {
 	echo "VARIANT=$VARIANT" >> $GITHUB_ENV
 	echo "patch_version=1" >> $GITHUB_ENV
 
-	LSPatch_dl
-	dl_gh "NexAlloy" "gnadgnaoh" "prerelease"
+	xposed_dl
 	# Patch Messenger:
 	get_apk "com.facebook.orca" "messenger-arm64-v8a" "bundle" "arm64-v8a" "nodpi" "Android 9+"
 	lspatch "messenger-arm64-v8a" "NexAlloy*.apk" "gnadgnaoh"
+	xposed_disable_play_store_updates "messenger-arm64-v8a"
 }
 
 instagram() {
@@ -41,11 +61,11 @@ instagram() {
 	echo "VARIANT=$VARIANT" >> $GITHUB_ENV
 	echo "patch_version=1" >> $GITHUB_ENV
 
-	LSPatch_dl
-	dl_gh "NexAlloy" "gnadgnaoh" "prerelease"
+	xposed_dl
 	# Patch Instagram:
 	get_apk "com.instagram.android" "instagram-arm64-v8a" "bundle" "arm64-v8a" "120-640dpi"  "Android 9.0+"
 	lspatch "instagram-arm64-v8a" "NexAlloy*.apk" "gnadgnaoh"
+	xposed_disable_play_store_updates "instagram-arm64-v8a"
 }
 
 case "$1" in
